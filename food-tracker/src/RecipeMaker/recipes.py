821b766d08b134126cb561031a08d41069518197
Pguin12 <74618829+Pguin12@users.noreply.github.com>
2025-09-27 18:23:05 -0400

    Attempting to get database working

--- food-tracker/src/RecipeMaker/recipes.py
+++ food-tracker/src/RecipeMaker/recipes.py
@@ -1,15 +1,35 @@
 import google.generativeai as genai
 from google.generativeai.types import GenerateContentResponse
+import os
+from dotenv import load_dotenv
+import csv
+from pymongo import MongoClient
+load_dotenv()
+genai.configure(api_key=(os.getenv("APIKEY")))
+
+client = MongoClient("mongodb://localhost:27017/")
+csv_file = (r"recipes.csv")
+
+db = client["Recipes"]
+collection = db["Recipes"]
 
 
-genai.configure(api_key="AIzaSyBCn45I0DUKZFUkHYaARAMzTe8EET_IWD0")
-
-
+with open(csv_file, "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        collection.insert_one(row)
+        
+for i, j in collection.find():
+    print(j)
+    if i == 10:
+        break
+    
+    
 class GeminiClient:
     def __init__(self):
         # Create an instance of the model you want to use.
         # The correct class is GenerativeModel.
-        self.model = genai.GenerativeModel("gemini-2.5-flash") # Using 1.5-flash as it's a common, solid choice
+        self.model = genai.GenerativeModel("gemini-2.5-flash")
         self.response: GenerateContentResponse | None = None
 
     def give_context(self, prompt: str) -> GenerateContentResponse:
@@ -19,9 +39,9 @@ class GeminiClient:
         return self.response
 
 
-# --- Example Usage ---
-if __name__ == "__main__":
-    my_client = GeminiClient()
-    response = my_client.give_context("Do you work?")
-    print(response.text)
+# # --- Example Usage ---
+# if __name__ == "__main__":
+#     my_client = GeminiClient()
+#     response = my_client.give_context("Do you work?")
+#     print(response.text)
     
